a685e8e64e21af2e61b550e47847dba06b312923
Karol Berezicki <karolberezicki@gmail.com>
2020-12-01 06:47:09 +0100

    2020 - Day01

--- AoC/2020/Day01/Day01.cs
+++ AoC/2020/Day01/Day01.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 
 namespace AoC._2020.Day01
 {
@@ -6,13 +9,48 @@ namespace AoC._2020.Day01
     {
         public void Execute()
         {
-            var input = Utils.LoadInputLines();
+            var expenses= Utils.LoadInputLines()
+                .SkipLast(1)
+                .Select(int.Parse)
+                .ToList();
 
-            var part1 = "";
-            var part2 = "";
+            var (part1, part2) = Solve(expenses);
 
-            Console.WriteLine($"Part1 {part1}");
+            Console.WriteLine($"Solve {part1}");
             Console.WriteLine($"Part2 {part2}");
+        }
+
+        [SuppressMessage("ReSharper", "ForCanBeConvertedToForeach")]
+        private static (int part1, int part2) Solve(IReadOnlyList<int> expenses)
+        {
+            var part1 = 0;
+            var part2 = 0;
+            for (var i = 0; i < expenses.Count; i++)
+            {
+                for (var j = 0; j < expenses.Count; j++)
+                {
+                    if (expenses[i] + expenses[j] == 2020)
+                    {
+                        part1 = expenses[i] * expenses[j];
+                    }
+
+                    for (var k = 0; k < expenses.Count; k++)
+                    {
+
+                        if (expenses[i] + expenses[j] + expenses[k] == 2020)
+                        {
+                            part2 = expenses[i] * expenses[j] * expenses[k];
+                        }
+
+                        if (part1 != 0 && part2 != 0)
+                        {
+                            return (part1, part2);
+                        }
+                    }
+                }
+            }
+
+            return (part1, part2);
         }
     }
 }
