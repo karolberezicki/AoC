621e62ec5dbef3e89db1bd9d2b2a0a136a772f21
Karol Berezicki <karolberezicki@gmail.com>
2020-12-03 06:38:06 +0100

    2020 Day03

--- AoC/2020/Day03/Day03.cs
+++ AoC/2020/Day03/Day03.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AoC._2020.Day03
 {
@@ -6,13 +8,33 @@ namespace AoC._2020.Day03
     {
         public void Execute()
         {
-            var input = Utils.LoadInputLines();
+            var input = Utils.LoadInputLines().SkipLast(1).ToList();
 
-            var part1 = "";
-            var part2 = "";
+            var part1 = Traverse(input, (3, 1));
+            var part2 = part1 * Traverse(input, (1, 1)) * Traverse(input, (5, 1)) * Traverse(input, (7, 1)) * Traverse(input, (1, 2));
 
             Console.WriteLine($"Part1 {part1}");
             Console.WriteLine($"Part2 {part2}");
+        }
+
+        private static long Traverse(IReadOnlyList<string> map, (int Right, int Down) slope)
+        {
+            var (right, down) = slope;
+            var counter = 0L;
+            var x = 0;
+            for (var y = 0; y < map.Count; y += down)
+            {
+                var mapLine = map[y];
+                if (mapLine[x] == '#')
+                {
+                    counter++;
+                }
+
+                x += right;
+                x = x >= mapLine.Length ? x - mapLine.Length : x;
+            }
+
+            return counter;
         }
     }
 }
