6d36bb9eb4ae73d419d2896739c6713f644fec75
Karol Berezicki <karolberezicki@gmail.com>
2020-12-06 06:21:00 +0100

    2020 - Day 05 and Day 06

--- AoC/2020/Day05/Day05.cs
+++ AoC/2020/Day05/Day05.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AoC._2020.Day05
 {
@@ -6,10 +8,63 @@ namespace AoC._2020.Day05
     {
         public void Execute()
         {
-            var input = Utils.LoadInputLines();
+            var input = Utils.LoadInputLines().SkipLast(1).ToList();
 
-            var part1 = "";
-            var part2 = "";
+            //input = new List<string>{ "FBFBBFFRLR" };
+
+            var seatIds = new List<int>();
+
+            foreach (var line in input)
+            {
+                var rowLowerRange = 0;
+                var rowUpperRange = 127;
+
+                for (var i = 0; i < 6; i++)
+                {
+                    if (line[i] == 'F')
+                    {
+                        rowUpperRange = (rowLowerRange + rowUpperRange) / 2;
+                        continue;
+                    }
+
+                    if (line[i] == 'B')
+                    {
+                        rowLowerRange = (rowLowerRange + rowUpperRange) / 2 + 1;
+                        continue;
+                    }
+                }
+
+                var row = line[6] == 'F' ? rowLowerRange : rowUpperRange;
+
+                var columnLowerRange = 0;
+                var columnUpperRange = 7;
+
+                for (var i = 7; i < 9; i++)
+                {
+                    if (line[i] == 'L')
+                    {
+                        columnUpperRange = (columnLowerRange + columnUpperRange) / 2;
+                        continue;
+                    }
+
+                    if (line[i] == 'R')
+                    {
+                        columnLowerRange = (columnLowerRange + columnUpperRange) / 2 + 1;
+                        continue;
+                    }
+                }
+
+                var column = line[9] == 'L' ? columnLowerRange : columnUpperRange;
+
+                var seatId = row * 8 + column;
+
+                seatIds.Add(seatId);
+
+            }
+
+            var part1 = seatIds.Max();
+            var part2 = Enumerable.Range(0, 128 * 8).Except(seatIds)
+                .First(i => i > 100 && i < 800);
 
             Console.WriteLine($"Part1 {part1}");
             Console.WriteLine($"Part2 {part2}");
--- AoC/2020/Day06/Day06.cs
+++ AoC/2020/Day06/Day06.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AoC._2020.Day06
 {
@@ -8,11 +10,46 @@ namespace AoC._2020.Day06
         {
             var input = Utils.LoadInputLines();
 
-            var part1 = "";
-            var part2 = "";
+            var lettersDictionary = GetLettersDict();
+            var group = 0;
+            var part1 = 0;
+            var part2 = 0;
+
+            foreach (var line in input)
+            {
+                if (line == string.Empty)
+                {
+                    part1 += lettersDictionary.Count(c => c.Value != 0);
+
+                    part2 += lettersDictionary.Count(c => c.Value == group);
+
+                    lettersDictionary = GetLettersDict();
+                    group = 0;
+                    continue;
+                }
+
+                group++;
+
+                foreach (var c in line)
+                {
+                    lettersDictionary[c] = lettersDictionary[c] + 1;
+                }
+            }
+
 
             Console.WriteLine($"Part1 {part1}");
             Console.WriteLine($"Part2 {part2}");
+        }
+
+        private static Dictionary<char, int> GetLettersDict()
+        {
+            var lettersDictionary = new Dictionary<char, int>();
+            for (var i = 'a'; i <= 'z'; i++)
+            {
+                lettersDictionary[i] = 0;
+            }
+
+            return lettersDictionary;
         }
     }
 }
