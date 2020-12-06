6c7c0fc6e15e7d25886a62069551ce90d9f57f92
Karol Berezicki <karolberezicki@gmail.com>
2020-12-06 21:38:50 +0100

    2020 Day05 - Refactor

--- AoC/2020/Day05/Day05.cs
+++ AoC/2020/Day05/Day05.cs
@@ -10,57 +10,7 @@ namespace AoC._2020.Day05
         {
             var input = Utils.LoadInputLines().SkipLast(1).ToList();
 
-            //input = new List<string>{ "FBFBBFFRLR" };
-
-            var seatIds = new List<int>();
-
-            foreach (var line in input)
-            {
-                var rowLowerRange = 0;
-                var rowUpperRange = 127;
-
-                for (var i = 0; i < 6; i++)
-                {
-                    if (line[i] == 'F')
-                    {
-                        rowUpperRange = (rowLowerRange + rowUpperRange) / 2;
-                        continue;
-                    }
-
-                    if (line[i] == 'B')
-                    {
-                        rowLowerRange = (rowLowerRange + rowUpperRange) / 2 + 1;
-                        continue;
-                    }
-                }
-
-                var row = line[6] == 'F' ? rowLowerRange : rowUpperRange;
-
-                var columnLowerRange = 0;
-                var columnUpperRange = 7;
-
-                for (var i = 7; i < 9; i++)
-                {
-                    if (line[i] == 'L')
-                    {
-                        columnUpperRange = (columnLowerRange + columnUpperRange) / 2;
-                        continue;
-                    }
-
-                    if (line[i] == 'R')
-                    {
-                        columnLowerRange = (columnLowerRange + columnUpperRange) / 2 + 1;
-                        continue;
-                    }
-                }
-
-                var column = line[9] == 'L' ? columnLowerRange : columnUpperRange;
-
-                var seatId = row * 8 + column;
-
-                seatIds.Add(seatId);
-
-            }
+            var seatIds = input.Select(GetSeatId).ToList();
 
             var part1 = seatIds.Max();
             var part2 = Enumerable.Range(0, 128 * 8).Except(seatIds)
@@ -68,6 +18,13 @@ namespace AoC._2020.Day05
 
             Console.WriteLine($"Part1 {part1}");
             Console.WriteLine($"Part2 {part2}");
+        }
+
+        private static int GetSeatId(string line)
+        {
+            var row = Convert.ToInt32(string.Join("", line.Take(7)).Replace('F', '0').Replace('B', '1'), 2);
+            var column = Convert.ToInt32(string.Join("", line.Skip(7).Take(3)).Replace('L', '0').Replace('R', '1'), 2);
+            return row * 8 + column;
         }
     }
 }
