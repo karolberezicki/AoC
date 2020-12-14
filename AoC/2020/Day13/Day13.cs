7a9e5a2b17080337359f831fde90b7123e9e7b19
Karol Berezicki <karolberezicki@gmail.com>
2020-12-14 15:27:31 +0100

    2020 Day13 - part 1

--- AoC/2020/Day13/Day13.cs
+++ AoC/2020/Day13/Day13.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 namespace AoC._2020.Day13
 {
@@ -6,9 +7,23 @@ namespace AoC._2020.Day13
     {
         public void Execute()
         {
-            var input = Utils.LoadInputLines();
+            var input = Utils.LoadInputLines().ToList();
 
-            var part1 = "";
+            var timestamp = int.Parse(input[0]);
+            var timetable = input[1]
+                .Split(",")
+                .Where(c => c != "x")
+                .Select(int.Parse)
+                .ToList();
+
+
+            var departureList = timetable.Select(id => (BusId: id, WaitTime: id - timestamp % id)).ToList();
+
+            var (busId, waitTime) = departureList.OrderBy(d => d.WaitTime).First();
+            var part1 = busId * waitTime;
+
+
+
             var part2 = "";
 
             Console.WriteLine($"Part1 {part1}");
