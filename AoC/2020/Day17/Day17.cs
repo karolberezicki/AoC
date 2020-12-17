0646769af5b71da8cd36dafc434a77836255a8cb
Karol Berezicki <karolberezicki@gmail.com>
2020-12-17 14:48:42 +0100

    2020 Day17

--- AoC/2020/Day17/Day17.cs
+++ AoC/2020/Day17/Day17.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AoC._2020.Day17
 {
@@ -6,13 +8,141 @@ namespace AoC._2020.Day17
     {
         public void Execute()
         {
-            var input = Utils.LoadInputLines();
+            var input = Utils.LoadInputLines().SkipLast(1).ToList();
 
-            var part1 = "";
-            var part2 = "";
+            var part1 = Part1(input);
+            var part2 = Part2(input);
 
             Console.WriteLine($"Part1 {part1}");
             Console.WriteLine($"Part2 {part2}");
+        }
+
+        private static int Part1(IReadOnlyList<string> input)
+        {
+            var grid = new Dictionary<(int X, int Y, int Z), bool>();
+
+            for (var y = 0; y < input.Count; y++)
+            {
+                for (var x = 0; x < input[y].Length; x++)
+                {
+                    if (input[y][x] == '#')
+                    {
+                        grid[(x, y, 0)] = true;
+                    }
+                }
+            }
+
+            for (var i = 0; i < 6; i++)
+            {
+                var surroundings = new HashSet<(int X, int Y, int Z)>();
+                foreach (var cube in grid)
+                {
+                    var cubeSurroundings = cube.Key.Around();
+                    surroundings.UnionWith(cubeSurroundings);
+                }
+
+                foreach (var cube in surroundings)
+                {
+                    if (!grid.ContainsKey(cube))
+                    {
+                        grid.Add(cube, false);
+                    }
+                }
+
+                var workingGrid = grid.ToDictionary(entry => entry.Key,
+                    entry => entry.Value);
+                foreach (var cube in grid)
+                {
+                    var active = cube.Key.Around().Count(c => Get3DGridValue(grid, c));
+
+                    if (cube.Value && (active == 2 || active == 3))
+                    {
+                        workingGrid[cube.Key] = true;
+                    }
+                    else if (cube.Value)
+                    {
+                        workingGrid[cube.Key] = false;
+                    }
+                    else if (!cube.Value && active == 3)
+                    {
+                        workingGrid[cube.Key] = true;
+                    }
+                }
+
+                grid = workingGrid;
+            }
+
+
+            return grid.Count(c => c.Value);
+        }
+
+        private static int Part2(IReadOnlyList<string> input)
+        {
+            var grid = new Dictionary<(int X, int Y, int Z, int W), bool>();
+
+            for (var y = 0; y < input.Count; y++)
+            {
+                for (var x = 0; x < input[y].Length; x++)
+                {
+                    if (input[y][x] == '#')
+                    {
+                        grid[(x, y, 0, 0)] = true;
+                    }
+                }
+            }
+
+            for (var i = 0; i < 6; i++)
+            {
+                var surroundings = new HashSet<(int X, int Y, int Z, int W)>();
+                foreach (var cube in grid)
+                {
+                    var cubeSurroundings = cube.Key.Around();
+                    surroundings.UnionWith(cubeSurroundings);
+                }
+
+                foreach (var cube in surroundings)
+                {
+                    if (!grid.ContainsKey(cube))
+                    {
+                        grid.Add(cube, false);
+                    }
+                }
+
+                var workingGrid = grid.ToDictionary(entry => entry.Key,
+                    entry => entry.Value);
+                foreach (var cube in grid)
+                {
+                    var active = cube.Key.Around().Count(c => Get4DGridValue(grid, c));
+
+                    if (cube.Value && (active == 2 || active == 3))
+                    {
+                        workingGrid[cube.Key] = true;
+                    }
+                    else if (cube.Value)
+                    {
+                        workingGrid[cube.Key] = false;
+                    }
+                    else if (!cube.Value && active == 3)
+                    {
+                        workingGrid[cube.Key] = true;
+                    }
+                }
+
+                grid = workingGrid;
+            }
+
+
+            return grid.Count(c => c.Value);
+        }
+
+        private static bool Get3DGridValue(IReadOnlyDictionary<(int X, int Y, int Z), bool> grid, (int X, int Y, int Z) position)
+        {
+            return grid.ContainsKey(position) && grid[position];
+        }
+
+        private static bool Get4DGridValue(IReadOnlyDictionary<(int X, int Y, int Z, int W), bool> grid, (int X, int Y, int Z, int W) position)
+        {
+            return grid.ContainsKey(position) && grid[position];
         }
     }
 }
--- AoC/UsefulExtensions.cs
+++ AoC/UsefulExtensions.cs
@@ -6,23 +6,66 @@ namespace AoC
     {
         public static IEnumerable<(int x, int y)> Around(this (int x, int y) point)
         {
-            yield return (point.x, point.y - 1);
-            yield return (point.x - 1, point.y);
-            yield return (point.x + 1, point.y);
-            yield return (point.x, point.y + 1);
+            var (x, y) = point;
+            yield return (x, y - 1);
+            yield return (x - 1, y);
+            yield return (x + 1, y);
+            yield return (x, y + 1);
+        }
+
+        public static IEnumerable<(int x, int y, int z)> Around(this (int x, int y, int z) point)
+        {
+            var (x, y, z) = point;
+            for (var dX = -1; dX <= 1; dX++)
+            {
+                for (var dY = -1; dY <= 1; dY++)
+                {
+                    for (var dZ = -1; dZ <= 1; dZ++)
+                    {
+                        if (dX == 0 && dY == 0 && dZ == 0)
+                        {
+                            continue;
+                        }
+                        yield return (x + dX, y + dY, z + dZ);
+                    }
+                }
+            }
+        }
+
+        public static IEnumerable<(int x, int y, int z, int w)> Around(this (int x, int y, int z, int w) point)
+        {
+            var (x, y, z, w) = point;
+            for (var dX = -1; dX <= 1; dX++)
+            {
+                for (var dY = -1; dY <= 1; dY++)
+                {
+                    for (var dZ = -1; dZ <= 1; dZ++)
+                    {
+                        for (var dW = -1; dW <= 1; dW++)
+                        {
+                            if (dX == 0 && dY == 0 && dZ == 0 && dW == 0)
+                            {
+                                continue;
+                            }
+                            yield return (x + dX, y + dY, z + dZ, w + dW);
+                        }
+                    }
+                }
+            }
         }
 
         public static IEnumerable<(int x, int y)> Adjacent(this (int x, int y) point)
         {
-            yield return (point.x, point.y - 1);
-            yield return (point.x - 1, point.y);
-            yield return (point.x + 1, point.y);
-            yield return (point.x, point.y + 1);
+            var (x, y) = point;
+            yield return (x, y - 1);
+            yield return (x - 1, y);
+            yield return (x + 1, y);
+            yield return (x, y + 1);
 
-            yield return (point.x - 1, point.y - 1);
-            yield return (point.x + 1, point.y - 1);
-            yield return (point.x - 1, point.y + 1);
-            yield return (point.x + 1, point.y + 1);
+            yield return (x - 1, y - 1);
+            yield return (x + 1, y - 1);
+            yield return (x - 1, y + 1);
+            yield return (x + 1, y + 1);
         }
     }
 }
