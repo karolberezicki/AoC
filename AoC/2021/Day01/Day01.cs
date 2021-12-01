51411370aa5bc05cbb96fa17daf3a8fd0f35e489
Karol Berezicki <karolberezicki@gmail.com>
2021-12-01 20:07:57 +0100

    2021 Day01

--- AoC/2021/Day01/Day01.cs
+++ AoC/2021/Day01/Day01.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AoC._2021.Day01
 {
@@ -6,13 +8,21 @@ namespace AoC._2021.Day01
     {
         public void Execute()
         {
-            var input = Utils.LoadInputLines();
+            var input = Utils.LoadInputLines().SkipLast(1).Select(int.Parse).ToList();
 
-            var part1 = "";
-            var part2 = "";
+            var part1 = Solve(input);
+            var part2 = Solve(input, 3);
 
             Console.WriteLine($"Part1 {part1}");
             Console.WriteLine($"Part2 {part2}");
+        }
+
+        private static int Solve(IReadOnlyCollection<int> input, int shift = 1)
+        {
+            return input
+                .Skip(shift)
+                .Zip(input, (curr, prev) => curr > prev ? 1 : 0)
+                .Sum();
         }
     }
 }
