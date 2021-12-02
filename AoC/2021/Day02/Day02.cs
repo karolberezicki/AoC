524ecd26bb2eef94a86fcd7b70de965747235b8e
Karol Berezicki <karolberezicki@gmail.com>
2021-12-02 08:35:03 +0100

    2021 Day02

--- AoC/2021/Day02/Day02.cs
+++ AoC/2021/Day02/Day02.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 namespace AoC._2021.Day02
 {
@@ -6,10 +7,35 @@ namespace AoC._2021.Day02
     {
         public void Execute()
         {
-            var input = Utils.LoadInputLines();
+            var input = Utils.LoadInputLines()
+                .SkipLast(1)
+                .Select(c => c.Split(' '))
+                .Select(c => (Action: c[0], Value: int.Parse(c[1])))
+                .ToList();
 
-            var part1 = "";
-            var part2 = "";
+            var horizontal = 0;
+            var aim = 0;
+            var depth = 0;
+
+            foreach (var (action, value) in input)
+            {
+                switch (action)
+                {
+                    case "forward":
+                        horizontal += value;
+                        depth += aim * value;
+                        break;
+                    case "down":
+                        aim += value;
+                        break;
+                    case "up":
+                        aim -= value;
+                        break;
+                }
+            }
+
+            var part1 = horizontal * aim;
+            var part2 = horizontal * depth;
 
             Console.WriteLine($"Part1 {part1}");
             Console.WriteLine($"Part2 {part2}");
