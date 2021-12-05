207e9093b1c57922212f3747b89a89f00575dadc
Karol Berezicki <karolberezicki@gmail.com>
2021-12-06 00:12:44 +0100

    2021 Day05 - input & parse

--- AoC/2021/Day05/Day05.cs
+++ AoC/2021/Day05/Day05.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 namespace AoC._2021.Day05
 {
@@ -6,7 +7,12 @@ namespace AoC._2021.Day05
     {
         public void Execute()
         {
-            var input = Utils.LoadInputLines();
+            var input = Utils.LoadInputLines().SkipLast(1)
+                .Select(l => l.Split(" -> ")
+                    .Select(c=> c.Split(',').Select(int.Parse).ToList()).ToList())
+                .Select(l => (X1: l[0][0], Y1: l[0][1], X2: l[1][0], Y2: l[1][1])).ToList();
+
+            var horizontalAndVerticalLines = input.Where(l => l.X1 == l.X2 || l.Y1 == l.Y2).ToList();
 
             var part1 = "";
             var part2 = "";
