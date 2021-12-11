2ada8684b905af3205bcdf9f8c2e46f18e57fd82
Karol Berezicki <karolberezicki@gmail.com>
2021-12-11 22:45:53 +0100

    2021 Day06 - input

--- AoC/2021/Day06/Day06.cs
+++ AoC/2021/Day06/Day06.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 namespace AoC._2021.Day06
 {
@@ -6,7 +7,7 @@ namespace AoC._2021.Day06
     {
         public void Execute()
         {
-            var input = Utils.LoadInputLines();
+            var input = Utils.LoadInput().Split(',').Select(int.Parse).ToList();
 
             var part1 = "";
             var part2 = "";
