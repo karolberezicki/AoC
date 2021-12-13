66135d1df9b87c327ec569c7a4831ccfaadf96bb
Karol Berezicki <karolberezicki@gmail.com>
2021-12-13 08:48:10 +0100

    2021 Day08 part 1

--- AoC/2021/Day08/Day08.cs
+++ AoC/2021/Day08/Day08.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AoC._2021.Day08
 {
@@ -6,9 +8,14 @@ namespace AoC._2021.Day08
     {
         public void Execute()
         {
-            var input = Utils.LoadInputLines();
+            var input = Utils.LoadInputLines()
+                .SkipLast(1)
+                .Select(l => l.Split(" | "))
+                .Select(c => (Signal: c[0].Split(" "), Output: c[1].Split(" "))).ToList();
 
-            var part1 = "";
+            var uniqueDigitsSegmentCounts = new List<int> { 2, 4, 3, 7 };
+            var part1 = input.SelectMany(l => l.Output).Count(o => uniqueDigitsSegmentCounts.Contains(o.Length));
+
             var part2 = "";
 
             Console.WriteLine($"Part1 {part1}");
