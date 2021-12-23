4f4c7885f0f123203db602b8eacc752bfd181940
Karol Berezicki <karolberezicki@gmail.com>
2021-12-23 19:30:03 +0100

    2021 Day09 part 1

--- AoC/2021/Day09/Day09.cs
+++ AoC/2021/Day09/Day09.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AoC._2021.Day09
 {
@@ -6,9 +8,34 @@ namespace AoC._2021.Day09
     {
         public void Execute()
         {
-            var input = Utils.LoadInputLines();
+            var input = Utils.LoadInputLines().SkipLast(1).ToList();
 
-            var part1 = "";
+            var map = new Dictionary<(int x, int y), int>();
+
+            for (var y = 0; y < input.Count; y++)
+            {
+                for (var x = 0; x < input[y].Length; x++)
+                {
+                    map[(x, y)] = input[y][x] - '0';
+                }
+            }
+
+            var risk = 0;
+            foreach (var point in map)
+            {
+                var pointsAroundCoordinates = point.Key.Around()
+                    .Where(p => p.x >= 0 && p.y >= 0 && p.x < input[0].Length && p.y < input.Count);
+
+                var pointsAroundValues = pointsAroundCoordinates.Select(p => map[p]).ToList();
+
+                var lowPoint = pointsAroundValues.All(p => p > point.Value);
+                if (lowPoint)
+                {
+                    risk += point.Value + 1;
+                }
+            }
+
+            var part1 = risk;
             var part2 = "";
 
             Console.WriteLine($"Part1 {part1}");
