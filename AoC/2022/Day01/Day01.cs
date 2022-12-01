5db4fec4762bc577bc643bd1d15423514e2f57a1
Karol Berezicki <karolberezicki@gmail.com>
2022-12-01 21:46:37 +0100

    2022 Day01

--- AoC/2022/Day01/Day01.cs
+++ AoC/2022/Day01/Day01.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AoC._2022.Day01
 {
@@ -8,8 +10,23 @@ namespace AoC._2022.Day01
         {
             var input = Utils.LoadInputLines();
 
-            var part1 = "";
-            var part2 = "";
+            var elfFood = new List<int>();
+            var tempElf = new List<int>();
+            foreach (var line in input)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    elfFood.Add(tempElf.Sum());
+                    tempElf.Clear();
+                }
+                else
+                {
+                    tempElf.Add(int.Parse(line));
+                }
+            }
+
+            var part1 = elfFood.Max();
+            var part2 = elfFood.OrderDescending().Take(3).Sum();
 
             Console.WriteLine($"Part1 {part1}");
             Console.WriteLine($"Part2 {part2}");
